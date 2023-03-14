83aa1427d1b1da677ce64ca51af670f1503d2e72
Kübra Aykan <aykan.kubra99@gmail.com>
2023-03-14 18:05:00 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double tutar, kdvOrani, kdvTutari,kdv1=0.18,kdv2=0.08,kdvliTutar;
+        Scanner input= new Scanner (System.in);
+        System.out.print("Ücret Tutarını Giriniz:");
+        tutar=input.nextDouble();
+        kdvOrani = tutar<=1000? kdv1:kdv2;
+        kdvTutari=tutar<=1000? tutar*kdv1:tutar*kdv2;
+        kdvliTutar=tutar+kdvTutari;
+
+        System.out.println("KDV'siz Tutar:"+tutar);
+        System.out.println("KDV Oranı:"+kdvOrani);
+        System.out.println("KDV Tutarı:"+kdvTutari);
+        System.out.println("KDV'li Tutar:"+kdvliTutar);
+
+
+
+
+
     }
 }
